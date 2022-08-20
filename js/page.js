88ecdc7f560d16494c2624e6938b82814b36a1cb
ADRIKASAHA <adrika.saha.55@gmail.com>
2022-08-21 01:05:51 +0600

    deposit

--- js/page.js
+++ js/page.js
@@ -1,5 +1,14 @@
-document.getElementById('btn').addEventListener('click',function(){
-    const depositField = document.getElementById('depositM');
-    const depositAmount = dipositField.value;
+document.getElementById('btnd').addEventListener('click',function(){
     
+    const depositField = document.getElementById('depositM');
+    const depositAmount = depositField.value;
+    const newDepositAmount = parseFloat(depositAmount); //to change input mood string to float
+    //console.log(newDepositAmount)
+    // console.log(depositAmount);
+    const depostiTotalElement = document.getElementById('dtotal');
+    const depostiTotal = depostiTotalElement.innerText;
+    const newDepositTotal = parseFloat(depostiTotal);
+    // depostiTotalElement.innerText = depositAmount;
+    const currentdTotle = newDepositAmount + newDepositTotal;
+    depostiTotalElement.innerText = currentdTotle;
 })
